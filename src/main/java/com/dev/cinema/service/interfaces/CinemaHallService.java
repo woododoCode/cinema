dd8e4e25c56a7714d403a47ad972067eeef88f5c
woododoCode <woododo2@gmail.com>
2020-05-31 20:40:30 +0300

    and they don't like me

--- src/main/java/com/dev/cinema/service/interfaces/CinemaHallService.java
+++ src/main/java/com/dev/cinema/service/interfaces/CinemaHallService.java
@@ -2,5 +2,5 @@ package com.dev.cinema.service.interfaces;
 
 import com.dev.cinema.model.CinemaHall;
 
-public interface CinemaHallService extends GenericService<CinemaHall>{
+public interface CinemaHallService extends GenericService<CinemaHall> {
 }

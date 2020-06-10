6e69bf2f75dfca2743eaf02b8686648ceb872dc9
woododoCode <woododo2@gmail.com>
2020-06-10 12:43:03 +0300

    Remove InternalResourceViewResolver method in WebConfig class

--- src/main/java/com/dev/cinema/config/WebConfig.java
+++ src/main/java/com/dev/cinema/config/WebConfig.java
@@ -1,21 +1,11 @@
 package com.dev.cinema.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @ComponentScan(basePackages = "com.dev.cinema.controllers")
 @EnableWebMvc
 public class WebConfig {
-
-    @Bean
-    public InternalResourceViewResolver getResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setViewClass(JstlView.class);
-        return resolver;
-    }
 }
